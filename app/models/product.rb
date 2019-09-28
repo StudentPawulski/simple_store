a86683727a303e8690e85ee7c2e0d13a05810251
spawulski@academic.rrc.ca <spawulski@MSI.localdomain>
2019-09-28 13:08:34 -0500

    tested validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :stock_quantity, presence: true
 end
