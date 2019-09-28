23bc0f98d6fa0ac3680cc02b5350e74d6aa39510
spawulski@academic.rrc.ca <spawulski@MSI.localdomain>
2019-09-28 14:06:21 -0500

    Finished assignment

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,8 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all # this instance variable will be shared
+    @count = Product.count
+    @average = Product.average(:price)
+    @sum = Product.sum(:stock_quantity)
   end
 end
