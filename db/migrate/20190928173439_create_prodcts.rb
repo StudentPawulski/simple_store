53a83289efdf1047fb939c22818e88d0b6b446b1
spawulski@academic.rrc.ca <spawulski@MSI.localdomain>
2019-09-28 12:35:58 -0500

    First commit

---
+++ db/migrate/20190928173439_create_prodcts.rb
@@ -0,0 +1,12 @@
+class CreateProdcts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :prodcts do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.integer :stock_quantity
+
+      t.timestamps
+    end
+  end
+end
